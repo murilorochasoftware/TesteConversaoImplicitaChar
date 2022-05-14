163f251f6647fe6a16489a0f38b0f3ee4449e822
Murilo Rocha <software@murilorocha.com.br>
2022-05-14 13:08:19 -0300

    Teste de conversão implícita de dados do tipo char para o tipo int em Java.

---
+++ src/TesteConversaoImplicitaChar.java
@@ -0,0 +1,10 @@
+public class TesteConversaoImplicitaChar {
+
+    public static void main(String[] args) {
+        char dadoChar = 'A';
+        int dadoInt = dadoChar;
+
+        System.out.printf("Dado char: %c", dadoChar);
+        System.out.printf("\nDado int: %d", dadoInt);
+    }
+}
